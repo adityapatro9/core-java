01789af32ab9406e9f73292823dcc6446c279b5c
Aditya <adityapatro3@gmail.com>
2022-11-10 19:18:12 +0530

    Added code -> for, adv for, forEach

--- src/streams/StreamsInitial.java
+++ src/streams/StreamsInitial.java
@@ -1,21 +1,30 @@
 package streams;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class StreamsInitial {
 
 	public static void main(String[] args) {
-		
-		List<Integer> al = new ArrayList<>();
-		al.add(9);
-		al.add(1);
-		al.add(10);
-		al.add(11);
-		al.add(0);
-		al.add(5);
-		
+
+		List<Integer> al = Arrays.asList(9, 1, 10, 11, 0, 5);
+
 		al.stream().sorted().forEach(System.out::println);
+		System.out.println("-------------------");
+		// Normal For loop
+		for (int i = 0; i < 6; i++) {
+			System.out.println(al.get(i));
+		}
+
+		System.out.println("-------------------");
+		// Advanced for loop
+		for (int i : al) {
+			System.out.println(i);
+		}
+
+		System.out.println("-------------------");
+		// ForEach
+		al.forEach(n -> System.out.println(n));
 
 	}
 
