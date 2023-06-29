d34e3d90e5639dabe5c62685d5671aa643f92f60
adityapatro9 <patroaditya9@gmail.com>
2023-06-29 14:03:49 +0530

    Added StringMemoryTest file

---
+++ src/strings/StringMemoryTest.java
@@ -0,0 +1,11 @@
+package strings;
+
+public class StringMemoryTest {
+
+	public static void main(String[] args) {
+		
+		// Code to test the memory allocation of strings, how we create it.
+
+	}
+
+}
